37040b8741ca11b557c7de80bd108e7654e64e33
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-11-30 11:39:39 -0700

    happy/sad paths

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'new user page' do
   before(:each) do
+    @user = User.create!(name: 'Sammy', email: 'sammysosa@yahoo.com')
     visit new_user_path
   end
 
@@ -13,5 +14,24 @@ RSpec.describe 'new user page' do
     click_link 'Home'
 
     expect(current_path).to eq(root_path)
+  end
+
+  it 'creates a new user' do
+    fill_in 'Name', with: @user.name
+    fill_in 'Email', with: @user.email
+    click_button 'Create Account'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content('New user created.')
+    expect(page).to have_content(@user.email)
+  end
+
+  it 'does not create user with missing fields' do
+    fill_in 'Email', with: @user.email
+    click_button 'Create Account'
+
+    expect(current_path).to eq(new_user_path)
+    expect(page).to_not have_content(@user.email)
+    expect(page).to have_content('Please try again.')
   end
 end
