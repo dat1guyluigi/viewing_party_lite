2059be1e0a588e3b7508a26ddf6ae040958813d4
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-11-30 11:37:58 -0700

    add create and new action with happy/sad paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+
   def new
-  end 
+    @user = User.new
+  end
+
+  def create
+    new_user = User.create(user_params)
+
+    if new_user.save
+      redirect_to root_path
+      flash[:notice] = 'New user created.'
+    else
+      redirect_to new_user_path
+      flash[:error] = 'Please try again.'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
