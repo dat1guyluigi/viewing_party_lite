f73f51ed7f788fd4ed08485fedc11738e64a9aff
Matthew Kimball <mekimba6@gmail.com>
2021-11-30 13:46:10 -0700

    adds user dashboard information and runs rubocop

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'show', type: :feature do
+  before(:each) do
+    @user = User.create!(name: 'Sammy', email: 'sammysosa@yahoo.com')
+    visit user_path(@user)
+  end
+  describe 'user show page' do
+    it 'has user information' do
+      expect(page).to have_content 'Welcome Sammy!'
+    end
+
+    it 'has a discover movies button' do
+      click_button 'Discover Movies'
+
+      expect(current_path).to eq(user_movies_path(@user))
+    end
+
+    it 'has a viewing parties section' do
+      
+      expect(page).to have_content "#{@user.name}'s Viewing Parties:"
+    end
+  end
+end
