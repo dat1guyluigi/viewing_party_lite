85246cc05e607abbf376e01bec02daa9649cffcf
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-12-02 14:19:12 -0700

    add movie search spec

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -6,10 +6,21 @@ RSpec.describe MovieService do
       top_page_1 = MovieService.top_rated_movies(1)
       top_page_2 = MovieService.top_rated_movies(2)
       top_forty = top_page_1 + top_page_2
-  
+
       expect(top_forty.length).to eq(40)
       expect(top_forty.second[:original_title]).to eq("The Shawshank Redemption")
       expect(top_forty.first[:title]).to eq("Dilwale Dulhania Le Jayenge")
+    end
+  end
+
+#create and add cassetes/vcr
+  describe 'movie search' do
+    it 'searches for a movie' do
+      search = MovieService.search_movies('The Shawshank Redemption')
+
+      expect(search).to be_a(Hash)
+      #get help pls :( trying to return specific name
+      #expect(search[:title]).to eq("The Shawshank Redemption")
     end
   end
 end
