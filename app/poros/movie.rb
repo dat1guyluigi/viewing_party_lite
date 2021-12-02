8ae7ba298115c85b602cc25368d39ef644709c32
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-12-01 22:01:13 -0700

    finish up movie poro attributes

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,13 +1,20 @@
-# class Movie
-#   attr_reader :title,
-#               :vote_average,
-#               :runtime,
-#               :genres,
-#               :summary,
-#               :cast,
-#               :reviews,
+class Movie
+  attr_reader :title,
+              :vote_average,
+              :runtime,
+              :genres,
+              :summary,
+              :cast,
+              :reviews
 
 
-#   def initialize(attributes)
-
-#   end
+  def initialize(data)
+    @title = data[:title]
+    @vote_average = data[:vote_average]
+    @runtime = data[:runtime]
+    @genres = data[:genres]
+    @summary = data[:summary]
+    @cast = data[:cast]
+    @reviews = data[:reviews]
+  end 
+end
