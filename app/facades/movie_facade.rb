0cd6c66f1bf6d05ed7fb7599c301dd9b346e4cab
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-12-02 14:18:00 -0700

    create movie facade with 2 endpoints

---
+++ app/facades/movie_facade.rb
@@ -0,0 +1,21 @@
+
+class MovieFacade
+  def self.top_movies
+    page_1 = MovieService.top_rated_movies(1)
+    page_2 = MovieService.top_rated_movies(2)
+
+    forty_movies = page_1 + page_2
+
+    forty_movies.map do |data|
+      Movie.new(data)
+    end
+  end
+
+  def self.movie_search(query)
+    search = MovieService.search_movies(query)
+
+    search[:results].map do |data|
+      Movie.new(data)
+    end
+  end
+end
