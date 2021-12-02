d534393dc902f70bf5405dcc9f38caf675d450bd
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-12-02 16:53:21 -0700

    add reviews to facade

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,5 +1,12 @@
 
 class MovieFacade
+
+  def self.reviews(movie_id)
+    ReviewService.review(movie_id)[:results].map do |review|
+      Review.new(review)
+    end
+  end
+
   def self.top_movies
     page_1 = MovieService.top_rated_movies(1)
     page_2 = MovieService.top_rated_movies(2)
