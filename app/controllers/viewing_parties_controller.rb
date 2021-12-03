4aae90499ad9cc141d6c007c8b8c349923b12f8d
Matthew Kimball <mekimba6@gmail.com>
2021-12-03 13:09:34 -0700

    adds a form for new parties

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -4,4 +4,14 @@ class ViewingPartiesController < ApplicationController
     @users = User.all
     @movie = MovieService.movie(params[:movie_id])
   end
+
+  def create
+    @party = Party.create(party_params)
+    require 'pry'; binding.pry
+  end
+
+  private
+  def party_params
+    params.require(:viewing_party).permit(:date, :time, :movie_id, :movie_title, :length, :host_id, :user_id)
+  end 
 end
