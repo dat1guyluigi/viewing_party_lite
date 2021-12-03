31045fd52244603bd2f12458efde01d65efe7d75
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-12-03 11:15:55 -0700

    destination migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_185857) do
+ActiveRecord::Schema.define(version: 2021_12_03_181459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2021_11_30_185857) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "time"
+    t.string "movie_title"
+    t.integer "movie_id"
+    t.integer "host_id"
     t.index ["user_id"], name: "index_parties_on_user_id"
   end
 
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,23 +2,52 @@ require 'rails_helper'
 
 RSpec.describe 'show', type: :feature do
   before(:each) do
-    @user = User.create!(name: 'Sammy', email: 'sammysosa@yahoo.com')
-    visit user_path(@user)
+    @user_1 = User.create!(name: 'Sammy', email: 'sammysosa@yahoo.com')
+    @user_2 = User.create!(name: 'Timmy', email: 'tim@yahoo.com')
+
+    @party_1 = Party.create!(date: '12/25/2021', length: 255, time: 1600, movie_id: 19404, host_id: @user_1.id, movie_title: "This movie")
+    @party_2 = Party.create!(date: '12/26/2021', length: 215, time: 1800, movie_id: 500, host_id: @user_2.id, movie_title: "That movie")
+
+    @userparty_1 = UserParty.create!(party_id: @party_1.id, user_id: @user_1.id)
+    @userparty_2 = UserParty.create!(party_id: @party_1.id, user_id: @user_2.id)
+    @userparty_3 = UserParty.create!(party_id: @party_2.id, user_id: @user_1.id)
+    @userparty_4 = UserParty.create!(party_id: @party_2.id, user_id: @user_2.id)
+
+    visit user_path(@user_1)
   end
   describe 'user show page' do
     it 'has user information' do
-      expect(page).to have_content 'Welcome Sammy!'
+      expect(page).to have_content "Welcome #{@user_1.name}!"
     end
 
     it 'has a discover movies button' do
       click_button 'Discover Movies'
 
-      expect(current_path).to eq(user_discover_path(@user))
+      expect(current_path).to eq(user_discover_path(@user_1))
     end
 
-    it 'has a viewing parties section' do
+    describe 'viewing party' do
+      it 'has a viewing parties section' do
+        expect(page).to have_content "#{@user_1.name}'s Viewing Parties:"
+      end
 
-      expect(page).to have_content "#{@user.name}'s Viewing Parties:"
+      it 'has movie info in viewing party' do
+        within("#id-#{@party_1.id}") do
+
+          expect(page).to have_content("Title: #{@party_1.movie_title}")
+          expect(page).to have_content("Length: #{@party_1.length}")
+          expect(page).to have_content("Date: #{@party_1.date}")
+          expect(page).to have_content("Time: #{@party_1.time}")
+        end
+      end
+
+      it 'links to individual movie' do
+        within("#id-#{@party_1.id}") do
+          click_link "This movie"
+
+          expect(current_path).to eq("/users/#{@user_1.id}/movies/#{@party_1.movie_id}")
+        end
+      end
     end
   end
 end
